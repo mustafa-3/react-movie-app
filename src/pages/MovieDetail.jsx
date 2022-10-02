a0e93f2a869d015b3827c4d01510c64c77740d61
mustafa-3 <sagunmustafa14@gmail.com>
2022-10-02 18:46:26 +0300

    v8

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -20,8 +20,8 @@ const MovieDetail = () => {
     movieInfo;
   return (
     <div className="card card-detail mb-3 display-6">
-      <div className="row g-0 flex-column ">
-        <div className="col-md-4 text-center w-100  ">
+      <div className="row g-0  ">
+        <div className="col-sm-12 col-lg-4 text-center ">
       
           <img
             src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
@@ -30,10 +30,10 @@ const MovieDetail = () => {
           />
         </div>
 
-        <div className="col-md-8  w-100">
-          <div className="card-body d-flex flex-column justify-content-center ">
+        <div className="col-sm-12 col-lg-8">
+          <div className="card-body  ">
              <h5 className="card-title">Overview</h5>
-            <p className="card-text mb-4">{overview}</p>
+            <p className="card-text ">{overview}</p>
             <p className="card-text">Release Date: {release_date}</p>
             <p className="card-text">Rate: {vote_average}</p>
             <p className="card-text">Total Vote: {vote_count}</p>

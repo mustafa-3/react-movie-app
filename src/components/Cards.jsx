12d6fb7ce5375312a6643954c6a81e1aea61c4ac
mustafa-3 <sagunmustafa14@gmail.com>
2022-10-02 13:56:08 +0300

    v4

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Cards = ({ movieData }) => {
-  const { title, poster_path, vote_average, overview } = movieData;
+  const { title, poster_path, vote_average, overview, id } = movieData;
+  const navigate = useNavigate()
   return (
     // <div className="col-sm-12 col-md-6 cold-lg-4 cursor-pointer">
     //   <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
     //   <h5>{title}</h5>
     // </div>
-    <div className="card col-sm-12 col-md-6 cold-lg-4 cursor-pointer m-4">
+    <div className="card col-sm-12 col-md-6 cold-lg-4 cursor-pointer m-4" onClick={() => navigate(`/detail/${id}`)}>
       <img
         src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
         className="card-img-top"
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const MovieDetail = ({movieData}) => {
+  const {id} = useParams()
+  const [movieInfo, setMovieInfo] = useState()
+  const API_KEY = "5c0e880db2a28d7094088d664c7cec1e";
+  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+  const getData = () => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => setMovieInfo(data))
+  }
   return (
     <div>MovieDetail</div>
   )
